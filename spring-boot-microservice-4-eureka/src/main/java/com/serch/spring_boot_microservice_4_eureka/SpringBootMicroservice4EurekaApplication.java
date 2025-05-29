f63522c27767015e5181cdc33d8aeaec6138380f
sguzman <serguz95444@gmail.com>
2025-05-29 13:53:22 -0600

    Creacion del service Eireka

---
+++ spring-boot-microservice-4-eureka/src/main/java/com/serch/spring_boot_microservice_4_eureka/SpringBootMicroservice4EurekaApplication.java
@@ -0,0 +1,13 @@
+package com.serch.spring_boot_microservice_4_eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMicroservice4EurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMicroservice4EurekaApplication.class, args);
+	}
+
+}
